b4e174c04f5d60eafe45d39d425aa31b75869968
Rishab Tiwari <rishabtiw045@gmail.com>
2025-09-09 11:39:11 +0530

    Initial commit - project setup with requirements.txt

---
+++ Template.sh
@@ -0,0 +1,11 @@
+mkdir -p ai_data_science_copilot/{data,src,notebooks} && \
+touch ai_data_science_copilot/data/raw_data.csv \
+ai_data_science_copilot/src/__init__.py \
+ai_data_science_copilot/src/data_loader.py \
+ai_data_science_copilot/src/preprocessor.py \
+ai_data_science_copilot/src/model_trainer.py \
+ai_data_science_copilot/src/main.py \
+ai_data_science_copilot/notebooks/01_initial_exploration.ipynb \
+ai_data_science_copilot/app.py \
+ai_data_science_copilot/requirements.txt \
+ai_data_science_copilot/README.md
